be9a542c160f6e529feeb5d50bff7bb56554aa1e
Martin Marcher <martin@marcher.name>
2011-12-30 15:56:06 +0100

    Initial commit

---
+++ README.rst
@@ -0,0 +1,1 @@
+cappedqueue has no README yet because the author sucks big time!
---
+++ template.go
@@ -0,0 +1,15 @@
+package cappedqueue
+
+import (
+	"fmt"
+)
+
+func main() {
+	Main()
+}
+
+func Main() {
+	fmt.Println("Hi there, I love cappedqueue")
+}
+
+// vim: set ts=4 sts=4 fenc=utf-8 noexpandtab list:
